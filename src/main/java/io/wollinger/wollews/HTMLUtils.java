d3d3238a7d59930ed06321120c430835db2e0be3
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2022-04-11 10:39:25 +0200

    Fixed issue with base path having two slashes

--- src/main/java/io/wollinger/wollews/HTMLUtils.java
+++ src/main/java/io/wollinger/wollews/HTMLUtils.java
@@ -9,9 +9,14 @@ public class HTMLUtils {
         StringBuilder response = new StringBuilder("<h3>Directory listing:</h3>");
         response.append("<ul>");
         for(File f : file.listFiles()) {
-            String link = requestInfo.getRequestedFile().getPath() + File.separator + f.getName();
+            //We do this to prevent baseFolder adding a double slash needlessly
+            String basePath = requestInfo.getRequestedFile().getPath();
+            if(basePath.equals("\\")) {
+                basePath = "";
+            }
+            String link = basePath + File.separator + f.getName();
             response.append("<li>");
-            response.append("<a href='").append(link).append("'>");
+            response.append("<a href='.").append(link).append("'>");
             response.append(f.getName());
             response.append("</a>");
             response.append("</li>");
